52e2c75ba0f5ea06983cb77da8c7a4a44cc4af71
huangqizai <17710946884@163.com>
2018-05-28 14:08:21 +0800

    Initial Commit

---
+++ task/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  task
+//
+//  Created by huangqizai on 2018/5/28.
+//  Copyright © 2018年 FYCK. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ task/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  task
+//
+//  Created by huangqizai on 2018/5/28.
+//  Copyright © 2018年 FYCK. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
